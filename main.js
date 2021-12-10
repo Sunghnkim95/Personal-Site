e40e0d85758341eb7c3f1c3f3e5e99b921f567de
Peter Kim <84411910+Sunghnkim95@users.noreply.github.com>
2021-12-09 19:37:23 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+function hover() {
+    document.getElementById("demo").innerHTML = "SUNG H. KIM";
+  }
+  function hoverback() {
+    document.getElementById("demo").innerHTML = "PETER KIM";
+  }
+  
+function hoverkor() {
+    document.getElementById("kor").innerHTML = "NAME: 김성훈";
+  }
+  function hoverkorback() {
+    document.getElementById("kor").innerHTML = "NAME: SUNG H. KIM";
+  }
